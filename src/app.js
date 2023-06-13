1354f300a8209776f0e910c3eaf1047fffdd1c85
JulioV10 <117667674+JulioV10@users.noreply.github.com>
2023-06-13 23:08:29 +0000

    randomcards

--- src/app.js
+++ src/app.js
@@ -2,10 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // write your code here
+  let icons = [
+    { icon: "♦", color: "red" },
+    { icon: "♥", color: "red" },
+    { icon: "♠", color: "black" },
+    { icon: "♣", color: "black" }
+  ];
+
+  let randomIndex = Math.floor(Math.random() * icons.length);
+  let randomIcon = icons[randomIndex];
+
+  let selectIcons = document.querySelectorAll(".symbol");
+  selectIcons.forEach(ele => {
+    ele.innerHTML = randomIcon.icon;
+    ele.style.color = randomIcon.color;
+  });
+
+  let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  let randomIndexNum = Math.floor(Math.random() * numbers.length);
+  let randomNumber = numbers[randomIndexNum];
+
+  let elementSelectNumber = document.querySelector(".number");
+  elementSelectNumber.innerHTML = randomNumber;
 };
